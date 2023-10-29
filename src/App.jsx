58661894943e40295066877e9f049a36cb7d65dc
Jed <kohjedrek@yahoo.com.sg>
2023-10-29 21:24:45 +0800

    Navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/NavBar";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
 
 export default App;
