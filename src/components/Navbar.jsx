f010be205155f291a5e4521c8456042ce649bcd6
Jed <kohjedrek@yahoo.com.sg>
2024-04-26 00:35:30 +0800

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,19 @@ import { links } from "../data";
 
 const Navbar = () => {
   return (
-    <nav className="bg-emerald-100 h-screen flex flex-col">
-      <div className="py-4 pb-80">
-        <h2 className="text-3xl font-bold">
-          Web<span className="text-emerald-600">Dev</span>
-        </h2>
-      </div>
-
-      <div className="flex flex-col gap-y-3">
+    <nav className="bg-stone-400 h-screen flex flex-col fixed left-0 top-0">
+      <div className="flex flex-col gap-y-28 mt-10 ml-4">
         {links.map((link) => {
           const { id, href, text } = link;
+          console.log(id);
           return (
-            <div key={id} className="vertical-text">
-              {text.split("").map((char, index) => (
-                <span
-                  key={index}
-                  className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
-                >
-                  {char}
-                </span>
-              ))}
-            </div>
+            <a
+              key={id}
+              href={href}
+              className="capitalize text-2xl text-stone-200 duration-75 hover:text-red-300 hover:text-3xl hover:cursor-pointer vertical-text transform -rotate-90 "
+            >
+              {text}
+            </a>
           );
         })}
       </div>
