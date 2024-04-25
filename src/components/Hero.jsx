988dcf32e165023f28448fd5ded4c5040f28fb85
Jed <kohjedrek@yahoo.com.sg>
2024-04-26 00:36:39 +0800

    Updated Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,25 @@
-import heroImg from "../assets/hero.svg";
 import { ReactTyped } from "react-typed";
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import particleConfig from "./particleConfig";
+import Particles from "react-tsparticles";
+
 const Hero = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-emerald-100">
+    <div className="flex items-center justify-center h-screen">
+      <Particles params={particleConfig} />
       <div className="max-w-4xl mx-auto px-4 py-8 sm:py-24 sm:px-6 lg:px-8 flex flex-col items-center text-center">
-        <h1 className="text-7xl font-bold tracking-wider">I'm Jedrek</h1>
+        <h1 className="text-7xl font-bold tracking-wider">
+          <span className="bg-gradient-to-r from-red-100 to-yellow-50 text-transparent bg-clip-text">
+            I'm Jedrek
+          </span>
+        </h1>
 
         <ReactTyped
-          className="mt-4 text-3xl text-slate-700 capitalize tracking-wide"
-          strings={["Aspiring Front End Developer", "Ttestinasd"]}
+          className="mt-4 text-2xl text-stone-200 capitalize tracking-wide font-medium"
+          strings={[
+            "Aspiring Front End Developer",
+            '<span class="text-red-900">Pokémon Card Collector ◓</span>',
+          ]}
           typeSpeed={45}
           backSpeed={20}
           loop
@@ -17,21 +27,16 @@ const Hero = () => {
 
         <div className="flex gap-x-4 mt-4 sm:justify-center">
           <a href="#">
-            <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            <FaGithubSquare className="h-8 w-8 text-red-900 hover:text-red-700 duration-300" />
           </a>
           <a href="#">
-            <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            <FaLinkedin className="h-8 w-8 text-red-900 hover:text-red-700 duration-300" />
           </a>
           <a href="#">
-            <FaTwitterSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            <FaTwitterSquare className="h-8 w-8 text-red-900 hover:text-red-700 duration-300" />
           </a>
         </div>
       </div>
-      {/* <img
-        src={heroImg}
-        className="hidden md:block absolute inset-y-0 right-0 h-screen"
-        alt="Hero"
-      /> */}
     </div>
   );
 };
