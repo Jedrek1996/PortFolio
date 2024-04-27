ba26078c09ca9cbf50b59786e4bd0a6e62f51cb7
Jed <kohjedrek@yahoo.com.sg>
2024-04-27 13:11:42 +0800

    Updated data

--- src/data.jsx
+++ src/data.jsx
@@ -1,32 +1,77 @@
 import { nanoid } from "nanoid";
-import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaJava,
+  FaPython,
+} from "react-icons/fa";
+import { FaGolang } from "react-icons/fa6";
+import { SiTailwindcss } from "react-icons/si";
+
+export const skills = [
+  {
+    id: nanoid(),
+    title: "HTML&CSS",
+    icon: <FaHtml5 className="h-16 w-16 text-blue-400" />,
+    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+  },
+  {
+    id: nanoid(),
+    title: "Javascript",
+    icon: <FaJs className="h-16 w-16 text-blue-400" />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+  },
+  {
+    id: nanoid(),
+    title: "React",
+    icon: <FaReact className="h-16 w-16 text-blue-400" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+  {
+    id: nanoid(),
+    title: "Tailwind",
+    icon: <SiTailwindcss className="h-16 w-16 text-blue-400" />,
+    text: "Proficient in Tailwind CSS, a utility-first CSS framework for rapidly building custom user interfaces.",
+  },
+  {
+    id: nanoid(),
+    title: "Node.js",
+    icon: <FaNodeJs className="h-16 w-16 text-blue-400" />,
+    text: "Skilled in Node.js, a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications.",
+  },
+  {
+    id: nanoid(),
+    title: "Go",
+    icon: <FaGolang className="h-16 w-16 text-blue-400" />,
+    text: "Familiar with Go, also known as Golang, an open-source programming language that makes it easy to build simple, reliable, and efficient software.",
+  },
+  {
+    id: nanoid(),
+    title: "Python",
+    icon: <FaPython className="h-16 w-16 text-blue-400" />,
+    text: "Experienced in Java, a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
+  },
+  {
+    id: nanoid(),
+    title: "Java",
+    icon: <FaJava className="h-16 w-16 text-blue-400" />,
+    text: "Experienced in Java, a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
+  },
+  {
+    id: nanoid(),
+    title: "JSP",
+    icon: <FaJava className="h-16 w-16 text-blue-400" />,
+    text: "Knowledgeable in JavaServer Pages (JSP), a technology that helps software developers create dynamically generated web pages based on HTML, XML, or other document types.",
+  },
+];
 
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
   { id: nanoid(), href: "#skills", text: "skills" },
   { id: nanoid(), href: "#about", text: "about" },
   { id: nanoid(), href: "#projects", text: "projects" },
-];
-
-export const skills = [
-  {
-    id: nanoid(),
-    title: "HTML&CSS",
-    icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
-    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
-  },
-  {
-    id: nanoid(),
-    title: "Javascript",
-    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
-    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
-  },
-  {
-    id: nanoid(),
-    title: "React",
-    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
-    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
-  },
 ];
 
 export const projects = [
