d8ce044e85a1b4aee8d1d263979253b238a9b18f
Jed <kohjedrek@yahoo.com.sg>
2024-04-28 11:56:21 +0800

    Updated navbar layout

--- src/components/Hero/Navbar.jsx
+++ src/components/Hero/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { links } from "../../data";
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 700);
+    };
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  console.log("isMobile:", isMobile);
+
   return (
-    <nav className=" h-screen flex flex-col fixed left-0 top-0">
+    <nav
+      className={`h-screen flex flex-col fixed left-0 top-0 ${
+        isMobile ? "sm:hidden" : ""
+      }`}
+    >
+      {isMobile && (
+        <button
+          className="text-blue-400 hover:text-blue-500 transition-all duration-500"
+          onClick={() => {
+            // Handle menu toggle logic here
+            console.log("Hamburger menu clicked");
+          }}
+        >
+          ☰
+        </button>
+      )}
       <div className="flex flex-col gap-y-28 mt-10 ml-2">
         {links.map((link) => {
           const { id, href, text } = link;
