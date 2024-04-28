93f083c5e77a4ac0ef9c996cc160c5788f09c2ce
Jed <kohjedrek@yahoo.com.sg>
2024-04-28 12:01:06 +0800

    Added Project component layout styles

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -5,7 +5,10 @@ import ProjectsCard from "./ProjectsCard";
 
 const Project = () => {
   return (
-    <section className="py-20 align-element" id="projects">
+    <section
+      className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 custom-smaller sm:w-5/12 md:w-8/12 lg:w-10/12 2xl:w-11/12 mx-auto align-element"
+      id="projects"
+    >
       <SectionTitle text="web creations"></SectionTitle>
       <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {
