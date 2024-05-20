8a9d6df6d1626d8f4f2a14401eae2cd8fd920e24
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 13:35:51 +0800

    Updated footer columns and colour

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-// import {
-//   RxDiscordLogo,
-//   RxGithubLogo,
-//   RxInstagramLogo,
-//   RxTwitterLogo,
-//   RxLinkedinLogo,
-// } from "rea";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-500 shadow-lg p-[15px] ">
+    <div className="w-full h-full bg-transparent text-white shadow-lg p-[15px] ">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-            <div className="font-bold text-[16px]">Community</div>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Youtube</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Github</span>
-            </p>
-            <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">Discord</span>
-            </p>
-          </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="font-bold text-[16px]">Social Media</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
