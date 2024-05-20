ba784260a209524edbb5451c0430511ef34f263d
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 13:36:04 +0800

    Updated Carousel settings

--- src/components/Skills/carouselSettings.jsx
+++ src/components/Skills/carouselSettings.jsx
@@ -8,12 +8,12 @@ export function SkillsNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <LiaChevronCircleRightSolid
-      className={`${className} absolute right-0 top-1/2 text-blue-400`}
+      className={`${className} absolute right-0 top-1/2`}
       style={{
         ...style,
         height: "20px",
         width: "20px",
-        color: "rgb(147 197 253 / var(--tw-text-opacity))",
+        color: "whitesmoke",
         right: "-50px",
         transition: "color 0.3s",
       }}
@@ -26,12 +26,12 @@ export function SkillsPrevArrow(props) {
   const { className, style, onClick } = props;
   return (
     <LiaChevronCircleLeftSolid
-      className={`${className} absolute top-1/2 transform -translate-y-1/2 text-blue-500`}
+      className={`${className} absolute top-1/2 transform -translate-y-1/2`}
       style={{
         ...style,
         height: "20px",
         width: "20px",
-        color: "rgb(147 197 253 / var(--tw-text-opacity))",
+        color: "whitesmoke",
         left: "-50px",
         transition: "color 0.3s",
       }}
