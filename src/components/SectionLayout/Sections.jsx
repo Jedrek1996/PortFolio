27ec6bf48a871e81ee727eed578316846e79387b
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 21:33:23 +0800

    Added new ProjecSlider section

--- src/components/SectionLayout/Sections.jsx
+++ src/components/SectionLayout/Sections.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Hero/Hero";
-import Skills from "../Skills/Skills";
+import Testimonials from "../Testimonials/Testimonials";
 import About from "../About/About";
 import Project from "../Projects/Project";
 import Experience from "../Experience/Experience";
 import Footer from "../Footer/Footer";
+import Skills from "../Skills/Skills";
+import ProjectSlider from "../Projects/ProjectSlider";
 
 function Sections() {
   return (
@@ -13,7 +15,8 @@ function Sections() {
       <About />
       <Experience />
       <Skills />
-      <Project />
+      {/* <Testimonials /> */}
+      <ProjectSlider />
       <Footer />
     </React.Fragment>
   );
