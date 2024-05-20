ff1c8a369a24d8f989bd83b67ceb07929cd25055
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 21:32:53 +0800

    Remove lottie animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SectionTitle from "../SectionLayout/SectionTitle";
 import { useInView } from "react-intersection-observer";
 import { hiddenStyles, showStyles } from "../CustomCss&Settings/observerStyles";
-import { aboutMe } from "../../../constants/data";
+import { aboutMe } from "../../assets/constants/data";
 import Lottie from "lottie-react";
 import morpheAnimation from "../../assets/animations/aboutMeAnimation.json";
 import "./about.css";
@@ -11,7 +11,7 @@ const About = () => {
   const { ref, inView } = useInView();
 
   return (
-    <div ref={ref} className="max-w-5xl mx-auto">
+    <div ref={ref} className="max-w-4xl mx-auto">
       <section
         className="py-32 about-section"
         id="about"
@@ -22,16 +22,16 @@ const About = () => {
             className="inline-block max-w-full"
             animationData={morpheAnimation}
           />
-          <article className="relative z-10 p-8 bg-white shadow-lg rounded-lg article-expand hover:scale-110">
-            <div className="flex items-center mb-8">
+          <article className="relative z-10 p-8 bg-white opacity-70 shadow-lg rounded-lg article-expand hover:scale-110">
+            <div className="flex items-center mb-8 opacity-100">
               <SectionTitle text="About Me" />
             </div>
             <div
-              className="text-slate-500 leading-loose font-medium text-lg"
+              className="text-slate-500 leading-loose font-medium text-base opacity-100"
               dangerouslySetInnerHTML={{ __html: aboutMe }}
             />
           </article>
-          <div className="absolute top-0 left-0 w-full h-full bg-blue-50 opacity-75 transform rotate-3 rounded-lg -z-10"></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-blue-100 opacity-30 transform rotate-3 rounded-lg -z-10"></div>
         </div>
       </section>
     </div>
