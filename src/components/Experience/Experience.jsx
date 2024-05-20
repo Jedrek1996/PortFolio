a1e0837d19db4d5f3e2214c7108a871509c381e8
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 10:17:08 +0800

    Updated experience styles

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionTitle from "../SectionLayout/SectionTitle";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { VscDebugStart } from "react-icons/vsc";
-import { experiencesData } from "../../data";
+
+import { experiencesData } from "../../../constants/data";
 
 function Experience() {
+  const [hoveredElement, setHoveredElement] = useState(null);
+
   const commonStyles = {
-    background: "rgb(147 197 253)",
-    color: " rgb(100 116 139)",
+    background: "rgb(169, 205, 246)",
+    color: "rgb(100 116 139)",
+  };
+
+  const hoverStyles = {
+    background: "rgb(100, 116, 139)",
+    color: "rgb(169, 205, 246)",
   };
 
   const commonClassNames = "vertical-timeline-element--work";
@@ -23,37 +30,38 @@ function Experience() {
       className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 custom-smaller sm:w-5/12 md:w-8/12 lg:w-10/12 2xl:w-11/12 mx-auto align-element"
     >
       <div>
-        <SectionTitle text="Experience"></SectionTitle>
+        <SectionTitle text="Experience" />
 
         <VerticalTimeline>
           {experiencesData.flat().map((experience, index) => (
             <VerticalTimelineElement
               key={index}
               className={commonClassNames}
-              contentStyle={commonStyles}
+              contentStyle={
+                hoveredElement === index ? hoverStyles : commonStyles
+              }
               contentArrowStyle={contentArrowStyle}
-              date="2011 - present"
-              iconStyle={commonStyles}
+              iconStyle={hoveredElement === index ? hoverStyles : commonStyles}
               icon={
                 <img
                   src={experience.icon}
                   className="rounded-full w-15 h-15 mx-auto"
                 />
               }
+              onMouseEnter={() => setHoveredElement(index)}
+              onMouseLeave={() => setHoveredElement(null)}
             >
-              <h3 className="vertical-timeline-element-title">
+              <h3 className="vertical-timeline-element-title experienceHeader">
                 {experience.position}
               </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                {experience.company}
+              <h4 className="vertical-timeline-element-subtitle text-xs experienceCompany">
+                {experience.company} ({experience.date})
               </h4>
-              <p>{experience.description}</p>
+              <p className="experienceBody font-normal">
+                {experience.description}
+              </p>
             </VerticalTimelineElement>
           ))}
-          <VerticalTimelineElement
-            iconStyle={commonStyles}
-            icon={<VscDebugStart />}
-          />
         </VerticalTimeline>
       </div>
     </section>
