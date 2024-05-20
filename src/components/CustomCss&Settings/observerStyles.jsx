de781949dc253086d313f3519473482489aaa665
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 10:16:57 +0800

    Added observer styles

--- src/components/CustomCss&Settings/observerStyles.jsx
+++ src/components/CustomCss&Settings/observerStyles.jsx
@@ -2,7 +2,7 @@ export const hiddenStyles = {
   opacity: 0,
   transform: "translateY(14px)",
   transitionProperty: "opacity, transform",
-  transitionDuration: "3s",
+  transitionDuration: "2.5s",
   transitionTimingFunction: "ease",
 };
 
@@ -10,7 +10,7 @@ export const showStyles = {
   opacity: 1,
   transform: "translateY(0)",
   transitionProperty: "opacity, transform",
-  transitionDuration: "5.9s",
+  transitionDuration: "3.9s",
   transitionTimingFunction: "ease",
 };
 
@@ -26,6 +26,46 @@ export const showHeroStyles = {
   opacity: 1,
   transform: "translateY(0)",
   transitionProperty: "opacity, transform",
-  transitionDuration: "5.9s",
+  transitionDuration: "3.9s",
   transitionTimingFunction: "ease",
+};
+
+export function slideInFromLeft(delay) {
+  return {
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export function slideInFromRight(delay) {
+  return {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export const slideInFromTop = {
+  hidden: { y: -100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+  },
 };
