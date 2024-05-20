8d4cb956ecb087a9ce42b041178c2bec38a4633e
Jed <kohjedrek@yahoo.com.sg>
2024-05-20 10:17:24 +0800

    Updated hero layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,35 @@
 import { ReactTyped } from "react-typed";
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import Lottie from "lottie-react";
+import heroAnimation from "../../assets/animations/heroAnimation.json";
+import "../../index.css";
 
 const Hero = () => {
   return (
-    <div
-      className={` items-center justify-center h-screen 
-        flex
-        }`}
-    >
+    <div className="flex items-center justify-center h-screen">
       <div className="max-w-4xl mx-auto px-4 py-8 sm:py-24 sm:px-6 lg:px-8 flex flex-col items-center text-center">
-        <h1 className="text-7xl font-bold tracking-wider">
-          <span className="bg-gradient-to-r from-blue-500 to-blue-200 text-transparent bg-clip-text">
-            I'm Jedrek
-          </span>
-        </h1>
+        <div className="relative flex justify-end w-full">
+          <h1 className="text-8xl font-thin tracking-wider">
+            <span className="bg-gradient-to-r from-blue-400 to-blue-200 text-transparent bg-clip-text inline-block animated-gradient-text z-30">
+              I'm Jedrek
+            </span>
+          </h1>
+          <div className="absolute bottom-8 -right-36 z-0">
+            <Lottie
+              className="inline-block"
+              animationData={heroAnimation}
+              style={{
+                width: "230px",
+                height: "auto",
+                opacity: 0.25,
+              }}
+            />
+          </div>
+        </div>
 
         <ReactTyped
           className="mt-4 text-xl text-blue-300 capitalize tracking-wide font-medium"
-          strings={[
-            "Software Developer",
-            '<span class="text-blue-300">Pokémon Card Collector ◓</span>',
-          ]}
+          strings={["Software Developer", "Pokémon Card Collector ◓"]}
           typeSpeed={45}
           backSpeed={20}
           loop
@@ -28,13 +37,13 @@ const Hero = () => {
 
         <div className="flex gap-x-4 mt-4 sm:justify-center">
           <a href="#">
-            <FaGithubSquare className="h-8 w-8 text-blue-200 hover:text-blue-300 duration-300" />
+            <FaGithubSquare className="h-8 w-8 text-red-200 hover:text-red-300 cursor-pointer duration-300" />
           </a>
           <a href="#">
-            <FaLinkedin className="h-8 w-8 text-blue-200 hover:text-blue-300 duration-300" />
+            <FaLinkedin className="h-8 w-8 text-blue-300 hover:text-blue-500 cursor-pointer duration-300" />
           </a>
           <a href="#">
-            <FaTwitterSquare className="h-8 w-8 text-blue-200 hover:text-blue-300 duration-300" />
+            <FaTwitterSquare className="h-8 w-8 text-green-300 hover:text-green-400 cursor-pointer duration-300" />
           </a>
         </div>
       </div>
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SectionTitle from "../SectionLayout/SectionTitle";
-import { projects } from "../../data";
+import { projects } from "../../../constants/data";
 import ProjectsCard from "./ProjectsCard";
 
 const Project = () => {
   return (
     <section
-      className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 custom-smaller sm:w-5/12 md:w-8/12 lg:w-10/12 2xl:w-11/12 mx-auto align-element"
+      className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 custom-smaller sm:w-5/12 md:w-8/12 lg:w-10/12 2xl:w-11/12 mx-auto align-element mt-72"
       id="projects"
     >
       <SectionTitle text="web creations"></SectionTitle>
