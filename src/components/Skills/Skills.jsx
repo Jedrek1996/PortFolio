6fc2409c05a945840fa8c442772657e5898bdfaa
Jed <kohjedrek@yahoo.com.sg>
2024-05-21 11:40:51 +0800

    Updated skills animation

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,29 +1,39 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 import { skillsData } from "../../assets/constants/data";
 import SkillDataProvider from "./SkillsDataProvider";
 
 const Skills = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
     <section
+      ref={ref}
       className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 custom-smaller sm:w-5/12 md:w-8/12 lg:w-10/12 2xl:w-11/12 mx-auto align-element mt-32"
-      id="projects"
+      id="skills"
     >
-      <div className="w-full flex flex-col justify-center items-center gap-5 text-center mt-24">
-        <div className="text-4xl font-light tracking-wider capitalize mb-8 animated-gradient-skills-section">
-          「Programming Skills」
+      {inView && (
+        <div className="w-full flex flex-col justify-center items-center gap-5 text-center mt-24">
+          <div className="text-4xl font-light tracking-wider capitalize mb-8 animated-gradient-skills-section">
+            「Technical Stack」
+          </div>
+          <div className="w-8/12 flex flex-row justify-around flex-wrap gap-5 items-center">
+            {skillsData.map((image, index) => (
+              <SkillDataProvider
+                key={index}
+                src={image.Image}
+                width={image.width}
+                height={image.height}
+                index={index}
+                name={image.skill_name}
+              />
+            ))}
+          </div>
         </div>
-        <div className="w-8/12 flex flex-row justify-around flex-wrap gap-5 items-center">
-          {skillsData.map((image, index) => (
-            <SkillDataProvider
-              key={index}
-              src={image.Image}
-              width={image.width}
-              height={image.height}
-              index={index}
-            />
-          ))}
-        </div>
-      </div>
+      )}
     </section>
   );
 };
--- src/components/Skills/SkillsDataProvider.jsx
+++ src/components/Skills/SkillsDataProvider.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import "./skills.css";
 
-const SkillDataProvider = ({ src, width, height, index }) => {
+const SkillDataProvider = ({ src, width, height, index, name }) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
 
-  const [isHovered, setIsHovered] = React.useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleHover = () => {
     setIsHovered(true);
@@ -21,7 +21,7 @@ const SkillDataProvider = ({ src, width, height, index }) => {
   return (
     <div
       ref={ref}
-      className="ripple-container"
+      className="relative transition-all duration-700 ease-in-out hover:-rotate-12 hover:scale-110"
       onMouseEnter={handleHover}
       onMouseLeave={handleHoverEnd}
     >
@@ -29,12 +29,23 @@ const SkillDataProvider = ({ src, width, height, index }) => {
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ delay: index * 0.1, duration: 0.5, ease: "easeInOut" }}
-        className="hover:scale-110 transition-transform duration-200"
+        whileHover={{ rotate: 5, scale: 1.1 }}
+        className="transition-transform duration-200 "
         style={{ display: "inline-block", position: "relative" }}
       >
         <img src={src} width={width} height={height} alt="skill image" />
       </motion.div>
-      {isHovered && <div className="ripple"></div>}
+
+      {isHovered && (
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+          className="absolute bottom-0 left-0 bg-[#7c1ba59e] text-white py-2 px-3 rounded-full text-sm"
+        >
+          {name}
+        </motion.div>
+      )}
     </div>
   );
 };
