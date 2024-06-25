c3ecc07b7ad7a9e4ec6dba8d120c66a244b0ed75
Jed <kohjedrek@yahoo.com.sg>
2024-06-25 13:27:34 +0800

    Update nav bar

--- src/components/Hero/Navbar.jsx
+++ src/components/Hero/Navbar.jsx
@@ -60,17 +60,31 @@ const Navbar = () => {
         {links.map((link, index) => {
           const { id, href, text, icon: Icon } = link;
           return (
-            <a
-              key={id}
-              href={href}
-              className={` capitalize text-2xl text-white hover:text-[#f0d5f5] transition-all duration-700 ease-in-out
-          hover:cursor-pointer hover:scale-110 ${
-            activeLink === index ? "text-pink-300 text-3xl" : ""
-          }`}
-              onClick={() => handleSetActiveLink(index)}
-            >
-              <Icon className="mr-2 inline-block ml-2" />
-            </a>
+            <div key={id} className="group">
+              <a
+                href={href}
+                className={`flex items-center capitalize text-2xl text-pink-100 font-extralight hover:text-[#f0d5f5] transition-all duration-700 ease-in-out
+                  hover:cursor-pointer hover:scale-110 ${
+                    activeLink === index
+                      ? "text-pink-300 text-3xl font-thin"
+                      : ""
+                  }`}
+                onClick={() => handleSetActiveLink(index)}
+              >
+                <Icon
+                  className="mr-2 inline-block ml-2 transition-shadow duration-300"
+                  style={{
+                    boxShadow: "1px 5px 30px rgba(127, 212, 240, 0.544)",
+                    backgroundColor: "transparent", 
+                    padding: "0", 
+                    borderRadius: "27%", 
+                  }}
+                />
+                <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  {text}
+                </span>
+              </a>
+            </div>
           );
         })}
       </div>
